763e8a6451f95dce45ee0a5db35faeb49a7e7f8a
SubhamPokhrel <subham.pokhrel@gmail.com>
2023-10-07 17:38:09 -0500

    bunch of erros

--- app/src/main/java/com/example/statefarmchallenge/MainActivity.java
+++ app/src/main/java/com/example/statefarmchallenge/MainActivity.java
@@ -1,4 +1,70 @@
 package com.example.statefarmchallenge;
 
-public class MainActivity {
+import android.util.Log;
+import android.widget.Button;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText editTextEmail, editTextPassword;
+    Button ButtonReg;
+    FirebaseAuth mAuth;
+    @Override
+    protected  void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.usernameField);
+        editTextPassword = findViewById(R.id.passwordField);
+        buttonReg = findViewById(R.id.button);
+
+        buttonReg.setOnClickListener(new View.OnClickListner()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                string email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if (TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT).sow();
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    toast.makeText(Register.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
+
+            }
+        });
+
+
+    }
+
 }
