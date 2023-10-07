b8d2698caa62739526ef2a34afca7e25819db63c
SubhamPokhrel <subham.pokhrel@gmail.com>
2023-10-07 18:10:34 -0500

    Register backend done

--- app/src/main/java/com/example/statefarmchallenge/MainActivity.java
+++ app/src/main/java/com/example/statefarmchallenge/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.statefarmchallenge;
 
+import static android.content.ContentValues.TAG;
+
+import android.annotation.SuppressLint;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -8,11 +13,19 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     TextInputEditText editTextEmail, editTextPassword;
-    Button ButtonReg;
+    Button buttonReg;
     FirebaseAuth mAuth;
+    @SuppressLint("WrongViewCast")
     @Override
     protected  void onCreate(Bundle savedInstanceState)
     {
@@ -23,40 +36,38 @@ public class MainActivity extends AppCompatActivity {
         editTextPassword = findViewById(R.id.passwordField);
         buttonReg = findViewById(R.id.button);
 
-        buttonReg.setOnClickListener(new View.OnClickListner()
+        buttonReg.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                string email, password;
+                String email;
+                String password;
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
 
                 if (TextUtils.isEmpty(email))
                 {
-                    Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT).sow();
+                    Toast.makeText(MainActivity.this,"Enter email",Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if(TextUtils.isEmpty(password))
                 {
-                    toast.makeText(Register.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this,"Enter Password", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    updateUI(user);
+                                    // Sign in success, updatupdateUI(null);e UI with the signed-in user's information
+                                    Toast.makeText(MainActivity.this, "Account Created",
+                                            Toast.LENGTH_SHORT).show();
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
-                                    updateUI(null);
                                 }
                             }
                         });
