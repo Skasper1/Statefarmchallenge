359a92a8c1dd76830dedc5ae20863333cf595680
Patrick W <134002574+Pat-TX@users.noreply.github.com>
2023-10-07 21:54:33 -0500

    working on weather app

---
+++ app/src/main/java/com/example/statefarmchallenge/Weather.java
@@ -0,0 +1,30 @@
+package com.example.statefarmchallenge;
+
+import static android.content.ContentValues.TAG;
+
+import android.annotation.SuppressLint;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Weather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
