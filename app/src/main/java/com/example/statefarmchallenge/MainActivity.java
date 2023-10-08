e02bf291fb685b411e8249c2c1ba5704848c277b
Chidiebere Emmanuel Ogba <ogbaemmanuel47@gmail.com>
2023-10-07 22:59:20 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/statefarmchallenge/MainActivity.java
+++ app/src/main/java/com/example/statefarmchallenge/MainActivity.java
@@ -1,15 +1,160 @@
 package com.example.statefarmchallenge;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
-public class MainActivity extends Activity
-{
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+
+public class MainActivity  extends AppCompatActivity {
+
+    private EditText Email,Password;
+    private Button LoginBtn, CreateNew, ResetPassword,button2;
+
+    //private MaterialButton CreateNew, ResetPassword;
+    private ProgressBar progressbar;
+
+
+    private FirebaseAuth mAuth;
+    
+
+    
+    public void CreateNew(View view){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // taking instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        // initialising all views through id defined above
+        Email = findViewById(R.id.Email);
+        Password = findViewById(R.id.Password);
+        LoginBtn = findViewById(R.id.LoginBtn);
+        progressbar = findViewById(R.id.progressBar);
+
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUserAccount();
+            }
+        });
+
+
+        
+
+
+
+
     }
+
+
+
+
+    private void loginUserAccount() {
+
+        // show the visibility of progress bar to show loading
+        progressbar.setVisibility(View.VISIBLE);
+
+        // Take the value of two edit texts in Strings
+        String email, password;
+        email = Email.getText().toString();
+        password = Password.getText().toString();
+
+
+        // validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // signin existing user
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(
+                                    @NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login successful!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+                                    // hide the progress bar
+                                    progressbar.setVisibility(View.GONE);
+
+                                    // if sign-in is successful
+                                    // intent to home activity
+                                    Intent intent
+                                            = new Intent(MainActivity.this,
+                                            Login.class);
+                                    startActivity(intent);
+                                }
+
+                                else {
+
+                                    // sign-in failed
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login failed!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+                                    // hide the progress bar
+                                    progressbar.setVisibility(View.GONE);
+                                }
+                            }
+                        });
+
+
+
+     /*   CreateNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "Testing Create button", Toast.LENGTH_SHORT).show();
+
+            }
+        });  */
+
+
+
+    }
+
+
+
 }
 
